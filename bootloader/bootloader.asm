6fceb8f028311df6b6490e7529caf7c450a6083c
Jonathon Roberts <jon@jonrob.net>
2017-11-10 20:59:08 +0000

    Bootloader loads sample.asm

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -36,10 +36,10 @@ ClrScr:
 	ret
 
 PutChar:
-	; print character 
+	; print character
 	mov ah, 0ah ; ah = 0ah - write character at cursor position
 	int 10h
-	
+
 	; Increment cursor position
 	add [_CurX], cx
 	mov dh, [_CurY]
@@ -63,12 +63,30 @@ Print:
 
 boot:
 	cli; disable interrupts
-	call ClrScr
-	mov si, msg	; SI points to message
-	call Print
+	cld; all that we need to init
+
+	mov ax, 0x50
+
+	;; set the buffer
+	mov es, ax
+	xor bx, bx
+
+	mov al, 2	; read sector 2
+	mov ch, 0	; track 0
+	mov cl, 2	; sector to read
+	mov dh, 0	; head number
+	mov dl, 0	; drive number
+
+	mov ah, 0x02	; read sectors from disk
+	int 0x13	; call the BIOS routine
+	jmp 0x50:0x0	; jmup and execute the sector
+;	call ClrScr
+
+;	mov si, msg	; SI points to message
+;	call Print
 
 halt:	hlt		; halt
-	
+
 	cld; all that we need to init
 	hlt; halt the system
 
