df5157b4a15cf3df242727d54780526d62bdb651
Jonathon Roberts <jon@jonrob.net>
2017-11-15 13:39:48 +0000

    Added defines

--- os/main.c
+++ os/main.c
@@ -3,23 +3,25 @@
 /* implement this */
 #include <amd64/i8259.h>
 
+#define VIDEO_MEM 0xB80000
+#define VIDEO_COLS 40
+#define VIDEO_ROWS 25
+
 void test64(){
 	__asm__ __volatile__(
 		"mov %rax, 0x01"/* x64 test */
 	);
 }
-void kprint( int colour, const char *string )
-{
-    volatile char *video = (volatile char*)0xB8000;
-    while( *string != 0 )
-    {
-        *video++ = *string++;
-        *video++ = colour;
-    }
+void kprint( int colour, const char *string ){
+volatile char *video = (volatile char*)0xB8000;
+	while(*string)
+	{
+		*video++ = *string++;
+		*video++ = colour;
+	}
 }
 
 int main(){
-	//kput();
 	kprint(2, "Hello World!");
 	test64();
 	__asm__("hlt");
