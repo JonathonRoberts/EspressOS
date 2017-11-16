7c18b800fe30fbe44be6ceb7eed0731bf7ffce1a
Jonathon Roberts <jon@jonrob.net>
2017-11-16 23:12:19 +0000

    Started libc

--- Makefile
+++ Makefile
@@ -1,27 +1,27 @@
 BUILD_DIR=build
 BOOTLOADER=$(BUILD_DIR)/bootloader/bootloader.o
-OS=$(BUILD_DIR)/os/os
+KERNEL=$(BUILD_DIR)/kernel/kernel
 FLOPPY_IMG=disk.img
 DISK_IMG=disk.iso
 
 all: bootdisk cdrom
 
-.PHONY: bootdisk bootloader os
+.PHONY: bootdisk bootloader kernel
 
 bootloader:
 	make -C bootloader
 
-os:
-	make -C os
+kernel:
+	make -C kernel
 
-bootdisk: bootloader os
+bootdisk: bootloader kernel
 	dd if=/dev/zero of=$(FLOPPY_IMG) bs=512 count=2880
 	dd conv=notrunc if=$(BOOTLOADER) of=$(FLOPPY_IMG) bs=512 count=1 seek=0
-	dd conv=notrunc if=$(OS) of=$(FLOPPY_IMG) bs=512 count=`du -k $(OS)|cut -f1` seek=1
+	dd conv=notrunc if=$(KERNEL) of=$(FLOPPY_IMG) bs=512 count=`du -k $(KERNEL)|cut -f1` seek=1
 
 cdrom:
 	cp $(FLOPPY_IMG) $(BUILD_DIR)/tmp
-	mkisofs -o $(DISK_IMG) -V OS -b $(FLOPPY_IMG) $(BUILD_DIR)/tmp
+	mkisofs -o $(DISK_IMG) -V KERNEL -b $(FLOPPY_IMG) $(BUILD_DIR)/tmp
 
 qemu-gdb:
 	qemu-system-x86_64 -machine q35 -cdrom $(DISK_IMG) -gdb tcp::26000 -S
@@ -30,5 +30,5 @@ qemu:
 
 clean:
 	make -C bootloader clean
-	make -C os clean
+	make -C kernel clean
 	rm $(BUILD_DIR)/tmp/*
---
+++ include/wctype.h
@@ -0,0 +1,32 @@
+/*
+ * The FUGPL License
+ * ===================
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software with only one restriction. No part of
+ * it may be included in software projects that are solely distributed under
+ * strong copyleft restricted licenses.  This license is *NOT* GPL compatible,
+ * and that is it's only restriction.
+ *
+ * There otherwise exists no restrictions in using this software, including
+ * without other limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+ #ifndef _WCTYPE_H_
+ #define _WCTYPE_H_
+ #endif
